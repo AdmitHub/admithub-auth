d2acfaff661459b03eacb4bd71eb362820865c2a
Charlie DeTar <cfd@media.mit.edu>
2015-05-11 15:07:53 -0600

    Fix pre-load error again

--- client/views/userMenu.js
+++ client/views/userMenu.js
@@ -1,7 +1,7 @@
 Template.userMenu.helpers({
   notificationCount: function() {
     var cursor = Herald.getNotifications({medium: 'onsite'});
-    var count = cursor ? cursor.count() : 0;
+    var count = cursor && cursor.count ? cursor.count() : 0;
     return {count: count};
   }
 });
