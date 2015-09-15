09ffadbf02700bd719edf6bff2ba89b111a25f5f
Charlie DeTar <cfd@media.mit.edu>
2015-09-14 18:34:34 -0600

    Update email hash property for telescope

--- server/hooks.js
+++ server/hooks.js
@@ -1,13 +1,14 @@
 Meteor.users.before.insert(function(userId, doc) {
   if (doc.emails && doc.emails[0] && doc.emails[0].address) {
-    doc.email_hash = Gravatar.hash(doc.emails[0].address);
+    doc.telescope = doc.telescope || {};
+    doc.telescope.emailHash = Gravatar.hash(doc.emails[0].address);
   }
 });
 
 Meteor.users.before.update(function(userId, doc, fieldNames, modifier, options) {
   if (_.contains(fieldNames, "emails") && modifier.$set &&
         doc.emails && doc.emails[0] && doc.emails[0].address) {
-    modifier.$set["email_hash"] = Gravatar.hash(doc.emails[0].address);
+    modifier.$set["telescope.emailHash"] = Gravatar.hash(doc.emails[0].address);
   }
 });
 
