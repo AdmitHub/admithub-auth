eb2830f2221c37cb15c4aea5935d4c4c92871014
Charlie DeTar <cfd@media.mit.edu>
2015-09-14 19:01:14 -0600

    Mirror emails.0.address to telescope.email

--- server/hooks.js
+++ server/hooks.js
@@ -2,6 +2,7 @@ Meteor.users.before.insert(function(userId, doc) {
   if (doc.emails && doc.emails[0] && doc.emails[0].address) {
     doc.telescope = doc.telescope || {};
     doc.telescope.emailHash = Gravatar.hash(doc.emails[0].address);
+    doc.telescope.email = doc.emails[0].address;
   }
 });
 
@@ -9,6 +10,7 @@ Meteor.users.before.update(function(userId, doc, fieldNames, modifier, options) 
   if (_.contains(fieldNames, "emails") && modifier.$set &&
         doc.emails && doc.emails[0] && doc.emails[0].address) {
     modifier.$set["telescope.emailHash"] = Gravatar.hash(doc.emails[0].address);
+    modifier.$set["telescope.email"] = doc.emails[0].address;
   }
 });
 
