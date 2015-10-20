7d1f67da898d5619328df0708476daa7dd0176dc
Pete Corey <petecorey@gmail.com>
2015-10-20 15:41:47 -0700

    Fixed Herald notification query

--- client/views/userMenu.js
+++ client/views/userMenu.js
@@ -1,6 +1,6 @@
 Template.userMenu.helpers({
   notificationCount: function() {
-    var cursor = Herald.getNotifications({medium: 'matchMessage'});
+    var cursor = Herald.collection.find({"media.matchMessage.send": true});
     var count = cursor && cursor.count ? cursor.count() : 0;
     return {count: count};
   }
