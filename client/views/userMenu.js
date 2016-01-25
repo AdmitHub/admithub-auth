66d3e1401549071e757b57ee8412d7be01f7cd2e
Pete Corey <petecorey@gmail.com>
2016-01-25 16:22:52 -0500

    Removing herald

--- client/views/userMenu.js
+++ client/views/userMenu.js
@@ -1,7 +1,12 @@
 Template.userMenu.helpers({
   notificationCount: function() {
-    var cursor = Herald.collection.find({"media.matchMessage.send": true});
-    var count = cursor && cursor.count ? cursor.count() : 0;
-    return {count: count};
+    /*
+     * TODO: Reimplement notification count
+     *
+     * Expects {
+     *   count: Number
+     * }
+     */
+    return 0;
   }
 });
